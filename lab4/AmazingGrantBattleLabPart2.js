0f786e34f490a360cb0b746888568f14442137fd
HollyJHuber <hollyjhuber@gmail.com>
2017-10-22 17:47:47 -0400

    added doBattle check, changed userHealthPoints < 1

--- lab4/AmazingGrantBattleLabPart2.js
+++ lab4/AmazingGrantBattleLabPart2.js
@@ -1,5 +1,5 @@
 
-  var userHealthPoints = 20;
+  var userHealthPoints = 40;
   var grantHealthPoints = 10;
   var userWins = 0;
   var userName;
@@ -13,7 +13,7 @@
     }
     else{
       console.log("what the hell is wrong with you?");
-      return 0;
+      return false;
     }
   }
 
@@ -22,61 +22,61 @@
     return Math.floor(Math.random()*5 +1);
   }
   
-  function startCombat(){
-    var continueCombat = prompt("Do you want to keep fighting? Y or N");
-    if (continueCombat.toUpperCase() === "Y"){
+  function continueCombat(){
+    var keepFighting = prompt("Do you want to attack [Y] or quit [N]?");
+    if (keepFighting.toUpperCase() === "Y"){
       return true;
     }
     else{
-      console.log("you chickened out!");
+      //console.log("you chickened out!");
       return false;
     }
   }
   
-
  if (startGame()){ 
    
     do{
       //DEBUG
-      console.log("doBattle: " + doBattle);
+      // console.log("doBattle: " + doBattle);
       
-      doBattle = 1;
+      doBattle = true;
         while( userHealthPoints >0 && grantHealthPoints >0 && doBattle){
           //battle rages on, decides who is the winner
           var randomNum = Math.random();
           var damageDealt = getDamage();
         
-          if (randomNum <0.5){
-            // Grant loses
-            grantHealthPoints -= damageDealt;
-            // console.log("AmazingGrant has " + grantHealthPoints+ " health points.");
+          if (randomNum > 0.5){
+            userHealthPoints -= damageDealt;  
+            console.log(userName + " has " + userHealthPoints+ " health points.");
+
           }
           else{
-            //user loses
-              userHealthPoints -= damageDealt;
-                // console.log(userName + " has " + userHealthPoints+ " health points.");
+            grantHealthPoints -= damageDealt;
+            console.log("AmazingGrant has " + grantHealthPoints+ " health points.");
           }
           
           // DEBUG
-          console.log("Grant: "+ grantHealthPoints +", " + userName + ": " + userHealthPoints + ", continue?");
-          // doBattle = startCombat();
+          // console.log("Grant: "+ grantHealthPoints +", " + userName + ": " + userHealthPoints + ", continue?");
+           
+           doBattle = continueCombat();
         }
         
-      if (userHealthPoints === 0) {
+      if (userHealthPoints < 1) {
         //game over
         console.log(userName + " won " + userWins + " battles but lost the war. AmazingGrant is the victor!");
         break;
+      } else if (userHealthPoints > 0 && !doBattle){
+        console.log("You have chosen to stop the fight. Game Over!");
+        break;
       }
       else{
+        console.log(userName + " has " + userWins + " win(s)!");
         userWins++;
         grantHealthPoints = 10;
       }
-      
-      if (userWins < 3)
-      console.log(userName + " has " + userWins + " win(s)!");
-      else{
-            console.log(userName + " has " + userWins + " wins and has emerged victorious!");
-      }
     }
-    while(userWins < 3);
+    while(userWins < 3 && doBattle);
+    if (userWins === 3){
+      console.log(userName + " has " + userWins + " wins and has emerged victorious!");
+    }
   }
