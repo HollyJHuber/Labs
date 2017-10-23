a2f87343337a155c57f7fe6731bdd8310646212f
HollyJHuber <hollyjhuber@gmail.com>
2017-10-22 21:25:20 -0400

    added comments, removed debug scoreboard

--- lab4/AmazingGrantBattleLabPart2.js
+++ lab4/AmazingGrantBattleLabPart2.js
@@ -1,10 +1,13 @@
+// initialize variables
 
   var userHealthPoints = 40;
   var grantHealthPoints = 10;
   var userWins = 0;
   var userName;
   var doBattle;
-  
+  var maximumWins = 3;
+
+// prompt user to play game & enter userName
   function startGame(){
     var wantToPlay = prompt("Do you want to play a game? Y or N");
     if (wantToPlay.toUpperCase() === "Y"){
@@ -17,31 +20,30 @@
     }
   }
 
-  // returns 1, 2, 3, 4, 5
+  // returns 1, 2, 3, 4, or 5
   function getDamage(){
     return Math.floor(Math.random()*5 +1);
   }
   
-  function continueCombat(){
+  function continueCombat(){ 
     var keepFighting = prompt("Do you want to attack [Y] or quit [N]?");
     if (keepFighting.toUpperCase() === "Y"){
       return true;
     }
     else{
-      //console.log("you chickened out!");
       return false;
     }
   }
-  
+
  if (startGame()){ 
-   
+   // game continues until AmazingGrant has been defeated maximumWins times
+
     do{
-      //DEBUG
-      // console.log("doBattle: " + doBattle);
-      
       doBattle = true;
-        while( userHealthPoints >0 && grantHealthPoints >0 && doBattle){
-          //battle rages on, decides who is the winner
+      // each battle takes place while both characters are still alive
+        while( userHealthPoints > 0 && grantHealthPoints > 0 && doBattle){
+          
+          // checks who takes damage and decrement HealthPoints accordingly
           var randomNum = Math.random();
           var damageDealt = getDamage();
         
@@ -54,13 +56,12 @@
             grantHealthPoints -= damageDealt;
             console.log("AmazingGrant has " + grantHealthPoints+ " health points.");
           }
-          
-          // DEBUG
-          // console.log("Grant: "+ grantHealthPoints +", " + userName + ": " + userHealthPoints + ", continue?");
-           
+
            doBattle = continueCombat();
         }
-        
+              
+      // breaks out of while loop when one player has reached <1 HealthPoints
+      // checks if user has chosen to quit  
       if (userHealthPoints < 1) {
         //game over
         console.log(userName + " won " + userWins + " battles but lost the war. AmazingGrant is the victor!");
@@ -70,13 +71,15 @@
         break;
       }
       else{
-        console.log(userName + " has " + userWins + " win(s)!");
+        // if user is still alive, they score one more victory and reset grantHealthPoints
         userWins++;
+        console.log(userName + " has won!");
         grantHealthPoints = 10;
       }
     }
-    while(userWins < 3 && doBattle);
-    if (userWins === 3){
-      console.log(userName + " has " + userWins + " wins and has emerged victorious!");
+    while(userWins < maximumWins && doBattle);
+    // when maximumWins is achieved, break the loop and display final message
+    if (userWins === maximumWins){
+      console.log(userName + " has emerged victorious! Game over.");
     }
   }
